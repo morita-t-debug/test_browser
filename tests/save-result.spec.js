07b6799b4d81922752f425ed58350a8cf26a6efe
morita <git config --global user.email>
2026-03-17 10:50:30 +0900

    複数ブラウザ表示対応バック側追加

--- tests/save-result.spec.js
+++ tests/save-result.spec.js
@@ -1,46 +1,45 @@
-import { test, expect } from '@playwright/test';
+import { test, chromium, firefox, webkit } from '@playwright/test';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
-test('サイトを開いて結果をSupabaseに保存する', async ({ page }) => {
-  const targetUrl = process.env.TARGET_URL || 'https://yahoo.co.jp';
-  let testStatus = 'success';
-  let screenshotBase64 = ''; // 画像データをいれる変数
+test('マルチブラウザテスト', async () => {
+  const targetUrl = process.env.TARGET_URL;
+  const browsers = process.env.ChOSEN_BROWSERS.split(',')
+  const takeScreenshot = process.env.TAKE_SCREENSHOT === 'true'; 
+  
 
   console.log(`テスト開始: ${targetUrl}`);
 
+  for (const browserType of browsers) {
+    console.log(`${browserType} でテスト開始...`);
+
+    //ブラウザの起動
+    const browser = await { chromium, firefox, webkit }[browserType].launch();
+    const page = await browser.newPage();
+  
   try {
     // 1. 指定されたURLにアクセス
     await page.goto(targetUrl, { waitUntil: 'networkidle' });
 
-    // 2. スクリーンショットを撮影し、Base64形式で取得
-    const buffer = await page.screenshot({ fullPage: true });
-    screenshotBase64 = buffer.toString('base64');
-
-  } catch (error) {
-    console.error('テスト失敗:', error);
-    testStatus = 'failure';
-  } finally {
-    // 3. Supabaseに結果と画像を保存
-    console.log(`Supabaseへ保存中... ステータス: ${testStatus}`);
+    // 2. スクリーンショットの必要、不必要を確認後、必要であれば、フルページ撮影しBase64形式で取得
+    let screenshotBase64 = null;
+    if (takeScreenshot) {
+      const buffer = await page.screenshot({ fullpage: true });
+      screenshotBase64 = buffer.toString('base64');
+    }
     
-    const { data, error } = await supabase
-      .from('test_result')
-      .insert([
-        {
-          url: targetUrl,
-          status: testStatus,
-          screenshot: screenshotBase64 
-        }
-      ]);
-
-    if (error) {
-      console.error('Supabase保存エラー:', error);
-    } else {
-      console.log('Supabaseへの保存が完了しました！');
+// Supabaseに保存（どのブラウザの結果か分かるように status にブラウザ名を入れるなど工夫）
+      await supabase.from('test_result').insert([{
+        url: targetUrl,
+        status: `${browserType}: success`,
+        screenshot: screenshotBase64
+      }]);
+      
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await browser.close();
     }
   }
 });
